d66ed47e38dbc6e31428926ae1572f61bb76043c
benkobalint1 <benkobalint1@gmail.com>
2026-05-12 21:48:36 +0200

    feat: add Spring Data JPA repositories for core entities

--- src/main/java/com/benkobalint1/expensetracker/domain/Household.java
+++ src/main/java/com/benkobalint1/expensetracker/domain/Household.java
@@ -21,7 +21,7 @@ public class Household {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
 
     @Column(nullable = false)
     private String name;
